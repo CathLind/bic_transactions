76182f13d94b48413b2fefcb670a5b3eb18ea828
Cath Lind <66523094+CathLind@users.noreply.github.com>
2020-10-08 20:26:29 +0200

    Update RenewMembershipBlock.php

--- src/Plugin/Block/RenewMembershipBlock.php
+++ src/Plugin/Block/RenewMembershipBlock.php
@@ -5,15 +5,9 @@ namespace Drupal\bic_transactions\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\transaction\TransactionServiceInterface;
-use Drupal\transaction\Plugin\Transaction\BalanceTransactor;
-use Drupal\transaction\TransactionInterface;
-use Drupal\transaction\TransactionTypeInterface;
-use Drupal\user\Entity\Role;
-use Drupal\user\RoleInterface;
+
 
 
 
@@ -27,42 +21,8 @@ use Drupal\user\RoleInterface;
  */
 class RenewMembershipBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
-  /**
-   * Drupal\Core\Session\AccountProxyInterface definition.
-   *
-   * @var \Drupal\Core\Session\AccountProxyInterface
-   */
-  protected $currentUser;
-
-    /**
-   * The transaction service.
-   *
-   * @var \Drupal\transaction\TransactionServiceInterface
-   */
-  protected $transactionService;
 
 
-  
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = new static($configuration, $plugin_id, $plugin_definition);
-    $instance->currentUser = $container->get('current_user');
-    return $instance;
-  }
-
-
-/**
- $target_user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-  if ($last_transaction = \Drupal::service('transaction')
-    ->getLastExecutedTransaction($target_user, 'userpoints_default_points')) {
-    $currentpoints = $last_transaction->get('field_userpoints_default_balance')->getString();
-  }
-**/
-  
-  
-  
   
   /**
    * {@inheritdoc}
